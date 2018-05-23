0f4c23cd2541db46b6f193f5233ec6b395cb6b6c
marinqwe <marinmarinbadrov@gmail.com>
2018-05-23 14:53:22 +0200

    Minor fix

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Redirect } from 'react-router-dom';
 import ListItem from './ListItem';
 import LoadingPage from './LoadingPage';
